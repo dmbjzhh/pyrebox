779a8821cacfdadc6486a841bde9642c237b6bb2
dmbjzhh <690276884@qq.com>
2018-09-21 17:43:44 +0800

    20180921

--- mdump.py
+++ mdump.py
@@ -3,10 +3,9 @@
 from __future__ import print_function
 import time
 from api import CallbackManager
-from ipython_shell import start_shell
 import api
-# from libdamm.api import API as DAMM
-# import sqlite3
+from libdamm.api import API as DAMM
+import sqlite3
 
 requirements = ["plugins.guest_agent"]
 
@@ -17,8 +16,10 @@ pyrebox_print = None
 
 target_procname = ""
 
-longest_time = 600
+longest_time = 20
+start_time = 0
 
+dump_path = "/home/sam/dump"
 
 def new_proc(params):
     '''
@@ -29,6 +30,7 @@ def new_proc(params):
     '''
     global pyrebox_print
     global cm
+    global start_time
 
     pid = params["pid"]
     pgd = params["pgd"]
@@ -37,12 +39,16 @@ def new_proc(params):
     if name.lower() == "malware.exe":
         pyrebox_print("Malware started! pid: %x, pgd: %x, name: %s" % (pid, pgd, name))
         cm.rm_callback("new_proc")
+        start_time = time.time()
         cm.add_callback(CallbackManager.BLOCK_END_CB, my_function, name="block_end")
         api.start_monitoring_process(pgd)
    
 def my_function(params):
+    global pyrebox_print
     global cm
-
+    global start_time
+    global longest_time
+    global dump_path
     # cpu_index = params["cpu_index"]
     # cpu = params["cpu"]
     # tb = params["tb"]
@@ -52,14 +58,38 @@ def my_function(params):
     # pgd = api.get_running_process(cpu_index)
     # pyrebox_print("Block end at (%x) %x -> %x\n" % (pgd, cur_pc, next_pc))
     pyrebox_print("damm is coming soon...")
+    end_time = time.time()
+    # if end_time - start_time >= longest_time:
+    #     pyrebox_print("analyze over :)")
+    #     cm.rm_callback("block_end")
     cm.rm_callback("block_end")
-    # damm = DAMM(plugins=['all'], memimg=os.path.abspath(dump_path+"thefile.dump"), profile="WinXPSP3x86",db=dump_path+"res0.db")
-    # results = damm.run_plugins()
-    # for elem in results:
-    #     print elem
+    damm = DAMM(plugins=['all'], profile="WinXPSP3x86", db=dump_path+"res0.db")
+    pyrebox_print("damm initialized")
+    results = damm.run_plugins()
+    for elem in results:
+        pyrebox_print(elem)
+    
     # sqlite_to_json(dump_path+"res0.db",dump_path+"res0.json")
     
-    
+def copy_execute(line):
+    '''Copy a file from host to guest, execute it, and pause VM on its EP
+
+       This file will be set as target, so that the script will start monitoring
+       context changes and retrieve the module entry point as soon as it is
+       available in memory. Then it will place a breakpoint on the entry point.
+    '''
+    global pyrebox_print
+    global target_procname
+    from plugins.guest_agent import guest_agent
+
+    pyrebox_print("Copying host file to guest, using agent...")
+    guest_agent.copy_file(line.strip(), "C:\\malware.exe")
+    guest_agent.execute_file("C:\\malware.exe")
+    guest_agent.stop_agent()
+
+    # Set target proc name:
+    target_procname = "malware.exe"
+    pyrebox_print("Waiting for process %s to start\n" % target_procname)
     
 
 def initialize_callbacks(module_hdl, printer):
@@ -75,9 +105,6 @@ def initialize_callbacks(module_hdl, printer):
     global pyrebox_print
 
     pyrebox_print = printer
-    pyrebox_print("[*] Snapshoting ...")
-    api.save_vm("isnapshot") # Noooooooo Waaaaaaaaaay
-    pyrebox_print("[*] Snapshot complete :)")
     pyrebox_print("[*]    Initializing callbacks")
     
     cm = CallbackManager(module_hdl, new_style = True)
@@ -87,46 +114,12 @@ def initialize_callbacks(module_hdl, printer):
 
 
 def clean():
-    pyrebox_print("[*] Recovering snapshot...")
-    api.load_vm("isnapshot")
-    pyrebox_print("[*] Recover snapshot complete:)")
     global cm
     print("[*]    Cleaning module")
     cm.clean()
     print("[*]    Cleaned module")
 
-
-def copy_execute(line):
-    '''Copy a file from host to guest, execute it, and pause VM on its EP - Custom command
-
-       This command will first use the guest agent to copy a file to the guest
-       and execute if afterwards.
-
-       This file will be set as target, so that the script will start monitoring
-       context changes and retrieve the module entry point as soon as it is
-       available in memory. Then it will place a breakpoint on the entry point.
-    '''
-    global pyrebox_print
-    global target_procname
-    from plugins.guest_agent import guest_agent
-
-    pyrebox_print("Copying host file to guest, using agent...")
-
-    guest_agent.copy_file(line.strip(), "C:\\malware.exe")
-    guest_agent.execute_file("C:\\malware.exe")
-    guest_agent.stop_agent()
-
-    # Set target proc name:
-    target_procname = "malware.exe"
-    pyrebox_print("Waiting for process %s to start\n" % target_procname)
-
-
 if __name__ == "__main__":
     print("[*] Loading python lalala module %s" % (__file__))
-   
-    # start_time = time.time()
-    # end_time = time.time()
-    # while end_time - start_time <= longest_time:
-    #     end_time = time.time()
     
     print("[*] hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
